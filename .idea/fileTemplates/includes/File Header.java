edc0b8b7124e8ad8c410a43cef462e3439a75d50
Shrawan Shinde <syntaxerror1972@gmail.com>
2022-09-25 12:54:59 +0530

    [Update] - added info

---
+++ .idea/fileTemplates/includes/File Header.java
@@ -0,0 +1,21 @@
+/**
+**************************************************************************************
+*
+*** Project Details ***
+*
+* Project Name : ${PROJECT_NAME}.
+* Package Id : ${PACKAGE_NAME}
+* Created By :  Shrawan Shinde 
+* Created Date :  ${DAY},${MONTH_NAME_FULL},${YEAR}
+*
+*** Contact Details ***
+*
+* Name : Shrawan B. Shinde
+* Email : syntaxerror1972@gmail.com
+* Github Link : https://github.com/syntaxerror1972/${PROJECT_NAME}
+* LinkedIn Link : https://in.linkedin.com/in/shrawan-shinde-59ba57a1
+* Copyright (c) ${YEAR}. All rights reserved.
+*
+**************************************************************************************
+*/
+
